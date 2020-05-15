39b27029eb5d6e34d561b6924eebcbb0f4f05c9d
kakkerenisujatha <65378395+kakkerenisujatha@users.noreply.github.com>
2020-05-15 23:37:33 +0100

    Update Test2.java

--- src/co/dataprovider/Test2.java
+++ src/co/dataprovider/Test2.java
@@ -3,7 +3,7 @@ package co.dataprovider;
 public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	System.out.println("testing changes");
+	System.out.println("techanges");
 
 
 }
